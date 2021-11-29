08dc5bf7c0612e5241d4555f4c0942d17fab9f45
ShchebetovaEK <polenka.92@list.ru>
2021-11-29 21:31:07 +0300

    Initial commit

---
+++ src/main/java/by/tms/medicins/entity/Version.java
@@ -0,0 +1,19 @@
+package by.tms.medicins.entity;
+
+public enum Version {
+    TABLET,
+    SUSPENSION,
+    DRAGEE,
+    CAPSULE,
+    SOLUTION,
+    PASTA,
+    GRANULE,
+    SYRUP,
+    DROPS,
+    EMULSION,
+    POWDER,
+    CREAM,
+    OINTMENT,
+    GEL,
+    SUPPOSITIRIES
+}
