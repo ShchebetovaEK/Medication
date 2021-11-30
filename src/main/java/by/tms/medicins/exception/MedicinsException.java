6e851e5850ae41995f3acc7d5cdfe9a393c7fbff
ShchebetovaEK <polenka.92@list.ru>
2021-11-30 22:48:32 +0300

    Initial commit

---
+++ src/main/java/by/tms/medicins/exception/MedicinsException.java
@@ -0,0 +1,15 @@
+package by.tms.medicins.exception;
+
+import java.io.IOException;
+
+public class MedicinsException extends IOException {
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+    }
+}
