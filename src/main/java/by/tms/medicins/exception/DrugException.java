7387609eaa780c22286fa9cccb543b476539649b
ShchebetovaEK <polenka.92@list.ru>
2021-12-13 10:22:39 +0300

    Initial commit

--- src/main/java/by/tms/medicins/exception/DrugException.java
+++ src/main/java/by/tms/medicins/exception/DrugException.java
@@ -1,4 +1,19 @@
 package by.tms.medicins.exception;
 
 public class DrugException extends Exception{
+    public DrugException() {
+        super();
+    }
+
+    public DrugException(String message) {
+        super(message);
+    }
+
+    public DrugException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DrugException(Throwable cause) {
+        super(cause);
+    }
 }
